d460882de496844c6aa2850a86362a0cb28e89d4
DrejT <vivekpatel4049@gmail.com>
2025-08-03 21:40:09 +0530

    done

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 // src/main.ts
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { WinstonLogger } from './logger/winston.logger';
 
 /**
  * The main bootstrap function for the NestJS application.
@@ -9,10 +10,14 @@ import { AppModule } from './app.module';
  */
 async function bootstrap() {
   // Create an instance of the NestJS application, passing the root module.
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    logger: WinstonLogger,
+  });
 
   // Get the port from environment variables or use a default of 3000.
   const port = process.env.PORT || 3000;
+  app.enableCors();
+  app.setGlobalPrefix('api');
 
   // Start the application and have it listen for incoming requests on the specified port.
   await app.listen(port);
